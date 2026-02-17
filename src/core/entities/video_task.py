2b7f3b0c8c79d942f0de0e72b9233675c27d0932
Philipp Hahmann <55932959+philipphahmann@users.noreply.github.com>
2026-02-16 23:47:23 -0300

    fix: correção da data de criação igual para todas as instâncias e inclusão do novo campo de s3_download_url no DynamoDB

--- src/core/entities/video_task.py
+++ src/core/entities/video_task.py
@@ -6,6 +6,7 @@ class VideoTask:
     id: str
     filename: str
     s3_path: str
+    s3_download_url: str
     status: str
     user_email: str
-    created_at: datetime = datetime.now()
+    created_at: datetime = field(default_factory=datetime.now)
