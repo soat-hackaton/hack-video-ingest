01393378a384d31c3c4bd417ddb31833ebbdf89c
Philipp Hahmann <55932959+philipphahmann@users.noreply.github.com>
2026-02-16 21:38:02 -0300

    fix: ajustado serviço do S3 para evitar erro de CORS

--- src/infra/aws/s3_service.py
+++ src/infra/aws/s3_service.py
@@ -4,16 +4,29 @@ from src.core.interfaces import StorageInterface
 from botocore.config import Config
 
 class S3Service(StorageInterface):
-    def __init__(self, bucket_name: str):
-        self.client = boto3.client('s3', region_name="us-west-2", config=Config(signature_version="s3v4"))
-        self.bucket = bucket_name
+    def __init__(self):
+        my_config = Config(
+            region_name="us-west-2",
+            signature_version="s3v4"
+        )
+
+        self.client = boto3.client('s3', config=my_config)
+        self.bucket = os.getenv("S3_BUCKET_NAME")
 
     def generate_presigned_url(self, key: str, content_type: str) -> str:
-        return self.client.generate_presigned_url(
-            'put_object',
-            Params={'Bucket': self.bucket, 'Key': key, 'ContentType': content_type},
-            ExpiresIn=3600
-        )
+        try:
+            return self.client.generate_presigned_url(
+                'put_object',
+                Params={
+                    'Bucket': self.bucket, 
+                    'Key': key, 
+                    'ContentType': content_type
+                },
+                ExpiresIn=3600
+            )
+        except Exception as e:
+            print(f"Erro ao gerar URL: {e}")
+            return None
 
     def file_exists(self, key: str) -> bool:
         try:
